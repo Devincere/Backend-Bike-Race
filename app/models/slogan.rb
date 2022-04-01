1451a30e488436fee807e4c60473b9377d616975
Vincent <71180739+devincere@users.noreply.github.com>
2022-04-02 00:52:33 +0100

    Fix slogan submission

--- app/models/slogan.rb
+++ app/models/slogan.rb
@@ -1,4 +1,4 @@
 class Slogan < ApplicationRecord
-  validates :firstName, presence: true, length: { minimum: 3, maximum: 10 }
-  validates :email, presence: true
+  #validates :firstName, presence: true, length: { minimum: 3, maximum: 10 }
+  #validates :email, presence: true
 end
